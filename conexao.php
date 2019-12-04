2d60b11d54faeac78f0fe41d167bc07978fcc03b
violet1001 <sabrinamtedesco@gmail.com>
2019-12-04 19:25:55 -0300

    v1

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+	$local="localhost:3306";
+	$usuario="root";
+	$senha="";
+	$bd="Madraj";
+	
+	$conexao=mysqli_connect($local,$usuario,$senha,$bd) or die("ERRO".mysqli_error($conexao));
+	mysqli_set_charset($conexao,"utf8");
+?>
---
+++ index.php
@@ -0,0 +1,42 @@
+		<?php include("head.inc"); ?>
+		<title>Home</title>
+	</head>
+
+	<body>
+		<div class="container-fluid">
+			<div id="filtro">
+				<h4 class="text-center">Filtro de estádios</h4>
+				<div class="container border border-dark p-3 text-center" id="opcoes">
+					Filtrar pelo time do estádio: <select name="time">
+						<?php
+							include("conexao.php");
+
+							$consulta="SELECT nome FROM estadio";
+							$resultado=mysqli_query($conexao,$consulta);
+
+							while($linha=mysqli_fetch_assoc($resultado)){
+								echo"<option value='".$linha["nome"]."'>".$linha["nome"]."</option>";
+							}
+						?>
+					</select>
+					Filtrar pelo time do estádio: 
+					<select name="time" id="time">
+						<?php
+							include("conexao.php");
+
+							$consulta="SELECT nome FROM time";
+							$resultado=mysqli_query($conexao,$consulta);
+
+							while($linha=mysqli_fetch_assoc($resultado)){
+								echo"<option value='".$linha["nome"]."'>".$linha["nome"]."</option>";
+							}
+						?>
+					</select>
+					<button class="btn btn-dark ml-3" id="filtro">Filtrar</button>
+				</div>
+					<br/>
+				<div id="estadios" class="container border border-dark" style="heigth:100px;"></div>
+			</div>
+		</div>
+	</body>
+</html>
